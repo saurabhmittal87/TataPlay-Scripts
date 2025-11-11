4c20cf61c7db18bbaed2825d0c66528822620f50
Saurabh Mittal <saurabh.mittal@tothenew.com>
2025-11-11 17:10:10 +0530

    update

--- src/main/java/com/tataplay/scripts/branches/CreateReleaseBranches.java
+++ src/main/java/com/tataplay/scripts/branches/CreateReleaseBranches.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public class CreateReleaseBranches {
 
-    private static String applicationList = "content-detail,notification-center,ta-client,homescreen-worker,unomailer";
-    private static String releaseBranchName = "release-30-10-2025-E";
+    private static String applicationList = "ext-config";
+    private static String releaseBranchName = "release-11-11-2025-E";
 
     public static void main(String... args) throws IOException {
         String[] applications = applicationList.split(",");
